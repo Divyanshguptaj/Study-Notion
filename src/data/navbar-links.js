12560d82c0f8e8ebbdd042673cf1509106f9ab03
Divyanshguptaj <luckys510039@gmail.com>
2025-02-18 23:22:34 +0530

    third commit , updated dashboard

--- src/data/navbar-links.js
+++
@@ -1,6 +0,0 @@
-export const NavbarLinks = [
-    {title: "Home", path: '/'},
-    {title: "Catalog", path: '/catalog'},
-    {title: "About", path: '/about'},
-    {title: "Contact Us", path: '/contact'}
-]
--- src/services/formatDate.js
+++
@@ -1,7 +0,0 @@
-export const formatDate = (dateString) => {
-    const options = { year: "numeric", month: "long", day: "numeric" }
-    const date = new Date(dateString)
-    const formattedDate = date.toLocaleDateString("en-US", options)
-  
-    return `${formattedDate}`
-  }

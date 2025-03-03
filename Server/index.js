acd7d5b1b3e50e4eb95d1d248a0233a97e055e83
Divyansh Gupta <157397799+Divyanshguptaj@users.noreply.github.com>
2025-03-03 18:27:23 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 //     origin: ["http://localhost:3000"],
 //     credentials: true,
 // }));
-app.use(cors({ origin: "https://study-notion-frontend-5an3rsc5e-divyansh-gupta-s-projects.vercel.app" }));
+app.use(cors({ origin: "https://study-notion-frontend-z0so.onrender.com/" }));
 
 app.use(fileUpload({useTempFiles:true,tempFileDir:"/tmp"}))
 cloudinaryConnect();

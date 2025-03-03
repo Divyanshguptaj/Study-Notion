ea02387b274105ab6312362b33aa15b20999c1e5
Divyansh Gupta <157397799+Divyanshguptaj@users.noreply.github.com>
2025-03-03 17:57:32 +0530

    Update apis.js

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_BACKEND_URL
+const BASE_URL = process.env.REACT_APP_BACKEND_URL || "https://study-notion-backend-g2t7.onrender.com"
 
 // AUTH ENDPOINTS
 export const endpoints = {
